397e11efd8129bd1c02c1a03becfd61e40ddc627
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-04-10 16:51:37 +0200

    init commint

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Ball from './Ball';
+
+
+class App extends Component {
+
+    render()
+    {
+        return (
+            <View>
+                <Ball />
+            </View>
+        );
+    }
+
+}
+
+export default App;
