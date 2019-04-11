363b5854bd48f054d5601b3c812a4fd0d473e0df
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-04-11 22:55:59 +0200

    added rotation and panResponder do Deck component

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,77 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { 
+    View,
+    Animated,
+    PanResponder
+ } from 'react-native';
+
 
 
 class Deck extends Component {
 
+    constructor(props)
+    {
+        super(props);
+        const position = new Animated.ValueXY();
+
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy });
+            },
+            onPanResponderRelease: () => {}
+        });
+
+        this.state = { panResponder, position };
+    }
+
+    getCardStyle()
+    {
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-500,0,500],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+        };
+    }
+
+
+
+    renderCards()
+    {
+        return this.props.data.map(
+            (item, index) => {
+                
+                if(index === 0)
+                {
+                    return (
+                        <Animated.View 
+                            key={ item.id } 
+                            style = { this.getCardStyle() }
+                            {...this.state.panResponder.panHandlers} 
+                        >
+                            { this.props.renderCard(item) }
+                        </Animated.View>
+                    
+                    );
+                }
+
+                return this.props.renderCard(item); 
+        });
+    }
+
+
     render()
     {
         return(
-            <View />
+            <View>
+                {this.renderCards()}
+            </View>
         );
     }
 
